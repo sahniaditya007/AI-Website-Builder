47b4880682051e1241d672f93097f0a34727ee23
Aditya Sahni <95126292+sahniaditya007@users.noreply.github.com>
2026-02-10 13:33:23 +0530

    feat: implement user routes for project management and credit handling

--- server/server.ts
+++ server/server.ts
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import cors from 'cors';
 import { toNodeHandler } from 'better-auth/node';
 import { auth } from './lib/auth';
+import userRouter from './routes/userRoutes';
 
 const app = express();
 
@@ -17,9 +18,13 @@ app.use(cors(corsOptions))
 
 app.all('/api/auth/{*any}', toNodeHandler(auth));
 
+app.use(express.json({limit: '50mb'}))
+
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
 });
+
+app.use('/api/user', userRouter);
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
